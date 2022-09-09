31c02d32175b4d13c295aed2e36d9a53d6072204
lucasdobleve <lucasernestowojcik@gmail.com>
2022-09-08 21:49:06 -0300

    estoy poniendo este archivo .js por primera vez

---
+++ strings_y_lenght.js
@@ -0,0 +1,10 @@
+var valores = ["verdadero", false, 2, "hola", "mundo", 3, "char"];
+ let mayor=0;
+for ([i=0]; []; [i++]){
+    valores[i].length;
+    if (valores[i].length>mayor){
+        valores[i]=mayor;
+    }
+
+}
+document.write("El mayor elemento de la lista es ", mayor);
